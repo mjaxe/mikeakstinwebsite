5a66abcf614fc41c14b0ba9fc6a6839b19254b4f
Michael Akstin <makstin33@gmail.com>
2025-06-26 16:20:32 -0400

    trying to add in my logo for testing

--- src/components/HeaderBar/HeaderBar.jsx
+++ src/components/HeaderBar/HeaderBar.jsx
@@ -1,9 +1,10 @@
 import './HeaderBar.css'
 import { Link } from 'react-router-dom'
-
+import logo from '../../assets/logo.png'
 function HeaderBar() {
     return (
         <nav className='headerbar'>
+            <img className='headerbar-logo' src={logo} alt='Header Bar Logo'/>
             <div className='headerbar-links'>
                 <Link className="link" to="/">Home</Link>
                 <Link className="link" to="/contact">Contact</Link>

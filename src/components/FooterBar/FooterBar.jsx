1d2075e4432602a9d699c31232a02ea05eaf485c
Michael Akstin <makstin33@gmail.com>
2025-06-26 11:17:46 -0400

    fixing the footbar typos

--- src/components/FooterBar/FooterBar.jsx
+++ src/components/FooterBar/FooterBar.jsx
@@ -2,7 +2,7 @@ import './FooterBar.css'
 
 function FooterBar() {
     return (
-        <div className='footebar'>
+        <div className='footerbar'>
             <p>Hello, I am going to be a footer bar!</p>
         </div>
     )

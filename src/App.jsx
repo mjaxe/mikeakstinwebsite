37836dbc60340f37d977decb15a8c49d11923993
Michael Akstin <makstin33@gmail.com>
2025-09-17 11:45:24 -0400

    removing email modal and including new email hyper link

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,11 @@ import Portfolio from './pages/Portfolio'
 import Academia from './pages/Academia'
 import Contact from './pages/Contact'
 import { Route, Routes } from 'react-router-dom'
-import { useState } from 'react'
-import Modal from './components/Modal/Modal'
 function App() {
-
-  const [isModalOpen, setModalOpen] = useState(false);
 
   return (
     <div className='app'>
-      <HeaderBar onOpenModal={() => setModalOpen(true)} className='app-headerbar'/>
+      <HeaderBar className='app-headerbar'/>
       <main className='main-content'>
         <Routes>
           <Route path='/' element={<Home/>}/>
@@ -26,7 +22,6 @@ function App() {
         </Routes>
       </main>
       <FooterBar className='app-footerbar'/>
-      <Modal isOpen={isModalOpen} onClose={() => setModalOpen(false)}/>
     </div>
   )
 }
